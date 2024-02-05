91a5cefff8d5f0833f3f0d6fd749d7c7269b1595
RohitKewat <developer@waxvapour.com>
2024-02-05 22:23:05 +0530

    added lecture 2

--- lecture2.js
+++ lecture2.js
@@ -1,2 +1,57 @@
 
+// function exampleFuction(a) {
+//          // funtion definition
+//          console.log("value of a is ",a) ;
+//          return a ;
+// }
+
+// exampleFuction(5) ; // function call
+
+// function squareRoot(a){   // a = 5
+//     let result = a*a ;    // result = 5 * 5 = 25 
+//     return result ;
+
+// }
+
+// // calling the function
+// let rootResult = squareRoot(5) ;
+// console.log("result of square root is ",rootResult) ;
+
+
+// let arrowFuntionExample = (a) => {
+//     // funtion body 
+//     let result = a * a ;
+//     return result
+// }
+
+// let resultOfArrowFuntion = arrowFuntionExample(6);
+
+// console.log(" Square root of given number is ", resultOfArrowFuntion)
+
+// hoisting in javascript 
+
+// console.log(hoistingExample) 
+
+// var hoistingExample = "Hello World" 
+
+// console.log(hoistingExample)
+
+// Scope There are two type of scope global and local variable
+
+// let a = 5
+
+// function exampleFuntion(a,b) {
+//                      // here  value of act as a local variable to funtion 
+//     console.log("value Of a ", a)
+//     console.log("value of B" , b)
+//     return true
+// }
+
+// exampleFuntion(6) ;
+
+
+let username = "Rohit"
+let mobileNumber = "123456789"
+
+console.log( `name of the User ${username}.the Mobile number of user is ${mobileNumber}`);
 
