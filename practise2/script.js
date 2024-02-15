48a68dcddb9ad816c5c5e45950be55ca747b354b
RohitKewat <developer@waxvapour.com>
2024-02-15 21:39:07 +0530

    made changes

---
+++ practise2/script.js
@@ -0,0 +1,49 @@
+
+window.addEventListener('load', loadImg);
+
+function loadImg() {
+
+  const url = "https://api.unsplash.com/search/photos?query=cat&per_page=20&client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k";
+
+    const imageDiv = document.getElementById('image');
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+
+                for (let i = 0; i < data.results.length; i++) {
+                    console.log(data.results[i].urls.small);
+                    /* Fetch only image that you want by using id. Example : https://unsplash.com/photos/6VhPY27jdps, id = '6VhPY27jdps'   */
+                        let imageElement = document.createElement('img');
+                        imageElement.src = data.results[i].urls.small;
+                        imageDiv.appendChild(imageElement);
+                }
+            });
+}
+
+const searchButton = document.getElementById('submit');
+searchButton.addEventListener('click', searchImage);
+ 
+function searchImage() {
+    const searchValue = document.getElementById('search').value;  //india
+
+    const url = `https://api.unsplash.com/search/photos?query=${searchValue}&per_page=20&client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k`;
+    const imageDiv = document.getElementById('image');
+    imageDiv.innerHTML = '';
+    
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+               
+                for (let i = 0; i < data.results.length; i++) {
+                    console.log(data.results[i].urls.small);
+                    /* Fetch only image that you want by using id. Example : https://unsplash.com/photos/6VhPY27jdps, id = '6VhPY27jdps'   */
+                        let imageElement = document.createElement('img');
+                        imageElement.src = data.results[i].urls.small;
+                        imageDiv.appendChild(imageElement);
+                }
+            });
+}
---
+++ practiseSession1.js
@@ -0,0 +1,46 @@
+
+// // Problem2 = You are given an array of integers, where each element represents the maximum number of steps you can take forward from that position. Write a function to determine if you can reach the last index starting from the first index.
+
+// //  [1, 1, 3, 2, 1, 1] = true
+// //  [1, 1, 0, 2, 1, 1] = false
+// //  [1, 1, 1, 3, 1, 1] = false
+
+
+
+// function stepCheckofArray(nums) {
+
+//     let arrayLength = nums.length -1 ;  // 5 
+
+
+//     for (let i = 0; i < nums.length-1; i++) {  // element = 1
+
+//         if (nums[i] > arrayLength) {    // 1 > 1  == false
+//             return false;
+//         }else if (nums[i] == 0 ){      // 1 ===0  == false
+//             return false;
+//         }
+//         arrayLength--;                   // arrayLength = 1-1 = 0
+//     }
+//  return true;
+// }
+
+// // Example usage:
+// const nums = [1, 1, 3, 4, 1, 1];
+
+// console.log(stepCheckofArray(nums)); 
+
+let url = "https://api.publicapis.org/entries" ;
+
+const AUTHHEADER = new Headers({'Content-Type': 'application/x-www-form-urlencoded'})
+const CONFIG = {
+    method: 'GET',
+    headers: AUTHHEADER,
+    mode: 'cors',
+}
+
+fetch(url,CONFIG).then((data)=>  data.json() ).then((data)=>{
+    console.log("api data",data);
+}).catch((error)=>{
+    console.log("error",error.message);
+})
+
