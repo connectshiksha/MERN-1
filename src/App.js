4ba329735ff180d0c80a24c203c0a64a6031d114
RohitKewat <developer@waxvapour.com>
2024-03-06 22:14:29 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Todo from "./components/Todo"
 import Form2 from "./components/Form"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RPSGame from './components/gameRPS';
 
 
 const App = () => {
@@ -21,6 +22,8 @@ const App = () => {
           <Route path="/" element={<Info />} />
           <Route path='/form' element={<Form2/>} />
           <Route path='/about' element={<AboutUs/>} />
+          <Route path='/game1' element={<RPSGame/>} />
+
 
 
           <Route path='*' element={ <>
---
+++ src/components/gameRPS.js
@@ -0,0 +1,63 @@
+import { Container, Row, Col, Image, Button } from "react-bootstrap"
+
+import paper from '../images/paper.png'
+import scissors from '../images/scissors.png'
+import stone from "../images/stone.png"
+import { useState } from "react"
+
+
+const RPSGame = () => {
+
+    const allInput = ["paper", "scissor", "stone"];
+    const [userInput ,setUserInput] = useState(null) ;
+    const [result ,setResult] = useState(null) ;
+
+
+    // handling user click 
+    const handleUserClick = (choosedChance) => {
+        setUserInput(choosedChance)
+
+    }
+    // generating random input from computer
+    
+    const generateComputerInput=()=>{
+     const randomInput =  Math.floor(Math.random() * (3)) + 0;
+      return  allInput[randomInput] ;
+    
+    }
+
+    const handlePlayBtn =()=>{
+       let comInput = generateComputerInput() ;
+
+       // you need to build logic according to game rule 
+       if(comInput ==="scissor" && userInput ==="stone" ) {
+          setResult("You Won !")
+       }else {
+        setResult("You Lost !!! ")
+       }
+ 
+    }
+
+    return (
+        <>
+            <Container style={{ margin: "5rem", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+                <div>
+                    <Image src={paper} alt="paper" fluid onClick={() => handleUserClick("paper")} height={200} width={200} />
+                    <Image src={scissors} alt="scissors" onClick={() => handleUserClick("scissor")} fluid height={200} width={200} />
+                    <Image src={stone} alt="stone" onClick={() => handleUserClick("stone")} fluid height={200} width={200} />
+                </div>
+
+                { userInput ? <Button variant="success" style={{ margin: "1rem" }} onClick={()=> handlePlayBtn()} > Play game</Button> : <h1>Please select the image </h1>}
+                {
+                    result ? <h1>
+                        {result}
+                    </h1>  :""
+                }
+
+            </Container>
+
+        </>
+    )
+}
+
+export default RPSGame 
